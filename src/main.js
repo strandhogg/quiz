57ba6999a2f32dbd78c156270dfcbe6aadfa41bf
strandhogg <matthiasengelhard20298@web.de>
2022-06-02 16:54:30 +0200

    new rename and added some questions

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            question: [
+                "Have you worked with a terminal/shell before?",
+                "Have you worked with linux before?",
+                "Have you worked with a SIEM system before?",
+                "Have you heard about IR before?",
+                "Have you attended a CR/CTF before?",
+                "What is an IP address?",
+                "What is a MAC address?",
+                ""
+            ]
+        }
+    }
+})
